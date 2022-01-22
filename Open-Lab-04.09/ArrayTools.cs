2b1c2125c4c9ce0ed189b751393e54bdef5ebe1c
janosheen <91180541+janosheen@users.noreply.github.com>
2022-01-22 11:33:38 +0100

    I implemented the method that removes all duplicate strings from an array.

--- Open-Lab-04.09/ArrayTools.cs
+++ Open-Lab-04.09/ArrayTools.cs
@@ -6,7 +6,25 @@ namespace Open_Lab_04._09
     {
         public string[] RemoveDups(string[] strings)
         {
-            throw new NotImplementedException();
+        bool alreadyExists;
+        string[] outputStrings = new string[]{};
+
+        for (int i = 0; i < strings.Length; i++)
+            {
+            alreadyExists = false;
+            for (int j = 0; j < outputStrings.Length; j++)
+                {
+                if (strings[i] == outputStrings[j])
+                alreadyExists = true;
+                }
+                    if (alreadyExists == false)
+                    {
+                    Array.Resize<string>(ref outputStrings, outputStrings.Length + 1);
+                    outputStrings[outputStrings.Length - 1] = strings[i];
+                    }
+            }
+            Console.WriteLine("Duplicate strings have been removed from this array");
+            return outputStrings;
         }
     }
 }
